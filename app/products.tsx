c66fe04094921d39741e167882ce8cc4dbcaf1a9
LuisCampana <luis.campana124@gmail.com>
2023-02-28 16:44:54 -0300

    cssproductsfinish

--- app/products.tsx
+++ app/products.tsx
@@ -8,7 +8,7 @@ export default function Items({ products }: Props) {
       {products.map((product) => (
         <div>
           <div key={product._id} className="border-2">
-            <div className="justify-center flex">
+            <div className="justify-center flex-wrap">
               <img
                 src={product.img.url as any}
                 alt={product.name}
@@ -17,17 +17,18 @@ export default function Items({ products }: Props) {
               />
             </div>
             <div className=" border-t-2 mt-[20px]">
-              <div className="mt-[15px] ml-[10px] mb-[15px] ">
-                <h2 className="text-[#222222] ">{product.name}</h2>
-                <p className="text-[#222222]">{product.category}</p>
+              <div className="mt-[15px] ml-[10px] mb-[15px]  ">
+                <h2 className="text-[#222222] w-auto ">{product.name}</h2>
+                <p className="text-[#857F7F]">{product.category}</p>
               </div>
             </div>
           </div>
-          <div className="border-2 rounded-[12px] flex justify-center p-[8px] mt-4  gap-[12px]  text-center ">
+          <div className="border-2 rounded-[12px] flex justify-center p-[8px] mt-4  gap-[12px]  text-center bg-[#810DDC]">
             <button className="">
-              <span className="text-[#222222] ">Comprar</span>
-              <>.</>
-              <span className="text-[#222222] pt-[40px]">${product.cost}</span>
+              <span className="text-[#FAF7F7]">Comprar</span>
+              <span className="text-[#FAF7F7] pt-[40px] ml-2">
+                ${product.cost}
+              </span>
             </button>
           </div>
         </div>
