d06751489cea166a4b31eebb212596b69ef515ba
LuisCampana <luis.campana124@gmail.com>
2023-03-03 16:23:42 -0300

    constts

---
+++ components/const.ts
@@ -0,0 +1,14 @@
+export const Todo_Filter = {
+  ALL: "all",
+  LAPTOPS: "laptops",
+  CAMERAS: "",
+  SMARTHOME: "smart home",
+  AUDIO: "audio",
+  MONITOR_TV: "monitores & tv",
+  PC_ACCESORIOS: "pc accesorios",
+  GAMING: "gaming",
+  TABLEST_E_READERS: "tablest & e-readers",
+  DRONES: "drones",
+  PHONES: "phones",
+  PHONES_ACCESORIOS: "phones accesorios",
+} as const;
